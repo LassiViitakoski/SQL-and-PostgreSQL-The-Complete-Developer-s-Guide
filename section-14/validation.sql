825136808e99d951df3b1968051109c66bc5c22d
LassiViitakoski <lassi.viitakoski@gmail.com>
2021-07-28 23:56:23 +0300

    Part 145 - Approaching More Complicated Designs - done

---
+++ section-14/validation.sql
@@ -0,0 +1,26 @@
+UPDATE products
+SET price = 999
+WHERE price is NULL;
+
+ALTER TABLE products
+ALTER COLUMN price
+SET NOT NULL;
+
+In this case you can add multiple same name or department
+but not row where name and department both are same as some other row
+ALTER TABLE products
+ADD UNIQUE(name, department);
+
+ALTER TABLE products
+DROP CONSTRAINT products_name_key;
+
+ALTER TABLE products
+ADD CHECK (price > 0);
+
+CREATE TABLE orders (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(40) NOT NULL,
+	created_at TIMESTAMP NOT NULL,
+	est_delivery TIMESTAMP NOT NULL,
+	CHECK (created_at < est_delivery)
+);
---
+++ section-15/readme.md
@@ -0,0 +1,3 @@
+# Section 15 - Database Structure Design Patterns
+
+## Let's build out a more serious version of an Instagram schema!
